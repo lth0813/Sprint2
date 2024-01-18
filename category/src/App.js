3fbec7103c5b721fd31857a3ba1ed6ab94bdc702
0shai0 <148052932+0shai0@users.noreply.github.com>
2024-01-18 14:51:15 +0900

    feat: 로딩 페이지 추가

--- category/src/App.js
+++ category/src/App.js
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import React from 'react';
 import './css/PhotoUpload.css';
+import './css/Loading.css';
 import './css/GhostLeg.css';
 import PhotoUpload from "./pages/PhotoUpload";
+import Loading from "./pages/Loading";
 import GhostLeg from "./pages/GhostLeg";
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<PhotoUpload/>}/>
+      <Route path='/loading' element={<Loading/>}/>
       <Route path='/ghostleg' element={<GhostLeg/>}/>
     </Routes>
   );
