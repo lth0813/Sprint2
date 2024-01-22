859eaf953767eb31febe08c131dc58b9aec6a653
lth0813 <lth0813@gmail.com>
2024-01-22 20:01:49 +0900

    01/22 1st

---
+++ S2 TEST/fe_test/src/App.js
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import './css/PhotoUpload.css';
+import './css/Loading.css';
+import './css/GhostLeg.css';
+import PhotoUpload from "./pages/PhotoUpload";
+import Loading from "./pages/Loading";
+import GhostLeg from "./pages/GhostLeg";
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<PhotoUpload/>}/>
+      <Route path='/loading' element={<Loading/>}/>
+      <Route path='/ghostleg' element={<GhostLeg/>}/>      
+    </Routes>
+  );
+}
+
+export default App;
